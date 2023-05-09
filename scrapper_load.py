04e5bf96d5c35e6e34426591b51e4f061182c257
Mylaana <alexandre.thore34@gmail.com>
2023-05-09 19:26:21 +0200

    ajout modules manquant

---
+++ scrapper_load.py
@@ -0,0 +1,53 @@
+"""module contenant les fonctions d'exportation (chargement)"""
+import shutil
+import csv
+import requests
+import scrapper_transform as transf
+
+#debug
+DEBUG = False
+
+
+def export_textfile(debug, directory, textfile_name, text_value):
+    """fonction d'exportation de fichier texte a l'emplacement désigné"""
+    if not debug:
+        return None
+    textfile_name = "export - " + textfile_name.lower()
+    with open(str(directory + "/").replace("//", "/") +
+              textfile_name.replace(".txt", "") + ".txt",
+              mode="w", encoding="utf-8") as output_textfile:
+        print("export : " + textfile_name + " as " + str(type(text_value)))
+        if isinstance(text_value, dict) or isinstance(text_value, list):
+            for line in text_value.items():
+                output_textfile.write(str(line) + "\n")
+        else:
+            output_textfile.write(str(text_value))
+
+
+def export_csv(data: dict, nom_fichier_csv: str, save_path: str):
+    """exportation d'un dictionnaire de donnée en format CSV"""
+
+    with open(save_path + nom_fichier_csv + ".csv", "w",encoding="utf-8") as fichier_csv:
+        writer = csv.writer(fichier_csv, delimiter=",")
+        for ligne in data:
+            writer.writerow(data[ligne].values())
+
+
+def export_img(image_url: str, image_nom: str, save_path: str):
+    """
+    exportation d'une image à partir du lien, vers le dossier spécifié
+    """
+
+    res = requests.get(image_url, stream = True, timeout= 10)
+    image_extension = R"." + image_url.split(R".")[-1]
+
+    #limitation du nombre de caractere dans le nom de l'image à 100
+    if len(image_nom) > 100:
+        image_nom = image_nom[0:99]
+
+    if res.status_code == 200:
+        with open(save_path + transf.no_char_spe(image_nom).
+                  replace(" ","-") + image_extension,'wb') as fichier_image:
+            shutil.copyfileobj(res.raw, fichier_image)
+    else:
+        print("impossible de télécharger l'image : " + image_nom)
---
+++ scrapper_transform.py
@@ -0,0 +1,57 @@
+"""importation des modules"""
+import re
+from urllib.parse import urljoin
+
+def no_char_spe(ma_chaine: str):
+    """
+    suppression des caracteres speciaux
+    """
+    resultat = re.sub(r"[^a-zA-Z0-9\s-]", "", ma_chaine)
+
+    return resultat
+
+
+def search_info(texte, start, end):
+    """
+    on recupere le substring "texte" situé dans le code html de la page,
+    situé entre les substring start et end
+    """
+    resultat = re.search(re.escape(start) + "(.+?)" +
+                         re.escape(end), str(texte))
+
+    if resultat:
+        resultat = resultat.group(1)
+
+    return resultat
+
+
+def transform_livre_info(livre_page_url, nom_cat, page_livre_soup):
+    """
+    recuperation de toutes les infos d'un livre depuis la page du livre
+    sous forme de dictionnaire.
+    """
+
+    livre_titre = str(page_livre_soup.title.get_text(strip=True)).split(
+        "|", maxsplit=1)[0].strip()
+    livre_upc = search_info(
+        page_livre_soup, "<th>UPC</th><td>", "</td>")
+    livre_prix_ht = search_info(
+        page_livre_soup, "<th>Price (excl. tax)</th><td>", "</td>").replace(r"Â","")
+    livre_prix_ttc = search_info(
+        page_livre_soup, "<th>Price (incl. tax)</th><td>", "</td>").replace(r"Â","")
+    livre_stock_nombre = search_info(
+        page_livre_soup, "stock (", " available)")
+    livre_description = str(page_livre_soup.find_all(
+        'meta', attrs={'name': 'description'})).split("\"")[1].replace(r"\n", "").strip()
+    livre_note = search_info(
+        page_livre_soup, "star-rating ", "\">")
+    livre_image_url = urljoin(
+        livre_page_url, page_livre_soup.find("img")["src"])
+
+    resultat = {"livre_url": livre_page_url, "upc": livre_upc,
+                "titre": livre_titre, "prix_ttc": livre_prix_ttc,
+                "prix_ht": livre_prix_ht, "stock": livre_stock_nombre,
+                "description": livre_description, "categorie": nom_cat,
+                "note": livre_note, "image_url": livre_image_url}
+
+    return resultat
